e165c9ece1ba5797dddbd41715fa69931fe62780
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-06-10 15:32:01 +0200

    Milestone 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,27 @@ import axios from 'axios';
 
 
 function App() {
+  // variabile di stato //
   const [actresses, setActresses] = useState([]);
 
   useEffect(() => {
-    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => {
-      const actressesList = resp.data.name;
-      setActresses(actressesList);
+    // chaiamata axios //
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+    .then((resp) => {
+      console.log("lista attrici ricevuta dall'API", resp.data);
+      setActresses(resp.data);
+      
     });
   }, []);
 
   return (
     <>
       <ul>
+        <h2>actresses list</h2>
         {actresses.map((curActress) => (
-          <li>{curActress.name}</li>
+          <li key={curActress.id}>
+            {curActress.name}
+          </li>
         ))}
       </ul>
     </>
